9bc2673edcb6f05a7d8458fd1bd735d2f8e95593
buildwithadithi <buildwithadithi@gmail.com>
2026-08-16 10:06:47 +0530

    Update classicKnapSack.cpp

--- 3DP/0-1KnapSack/classicKnapSack.cpp
+++ 3DP/0-1KnapSack/classicKnapSack.cpp
@@ -2,35 +2,36 @@
 using namespace std;
 
 class Solution {
-  public:
-
-    int solve(int i, int W, vector<int>& val,
+public:
+    int solve(int idx, int W, vector<int>& val,
               vector<int>& wt, vector<vector<int>>& dp) {
 
-        if (i == 0 || W == 0)
-            return dp[i][W] = 0;
+        // No items left
+        if (idx == val.size())
+            return 0;
 
-        if (dp[i][W] != -1)
-            return dp[i][W];
+        if (dp[idx][W] != -1)
+            return dp[idx][W];
 
+        // Skip current item
+        int notTake = solve(idx + 1, W, val, wt, dp);
+
+        // Take current item
         int take = 0;
+        if (wt[idx] <= W) {
+            take = val[idx] +
+                   solve(idx + 1, W - wt[idx], val, wt, dp);
+        }
 
-        if (wt[i - 1] <= W)
-            take = val[i - 1] +
-                   solve(i - 1, W - wt[i - 1], val, wt, dp);
-
-        int notTake = solve(i - 1, W, val, wt, dp);
-
-        return dp[i][W] = max(take, notTake);
+        return dp[idx][W] = max(take, notTake);
     }
 
     int knapsack(int W, vector<int>& val, vector<int>& wt) {
 
         int n = val.size();
 
-        vector<vector<int>> dp(n + 1,
-                               vector<int>(W + 1, -1));
+        vector<vector<int>> dp(n, vector<int>(W + 1, -1));
 
-        return solve(n, W, val, wt, dp);
+        return solve(0, W, val, wt, dp);
     }
 };
